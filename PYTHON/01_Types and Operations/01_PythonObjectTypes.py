c98055d6141203dd78e09e3ee86f9be703661ab5
YoujeongPark <pyj0695@gmail.com>
2021-07-17 10:28:28 +0900

    python types

---
+++ PYTHON/01_Types and Operations/01_PythonObjectTypes.py
@@ -0,0 +1,33 @@
+#Strings
+name = "Rosy"
+print(name) #Rosy
+print(type(name)) #<class 'str'>
+
+#Lists
+myList = []
+myList = [1,2,3,4,5]
+myList = [1,"Rosy",2.45]
+myList = [1,["2","3"],4] #nested List
+print(myList)
+print(type(myList))
+
+#Dictionaries
+myDic = {"rosy" : 20 , "job" : "developer"}
+print(myDic)
+print(type(myDic))
+
+#Tuples
+myTuple = ("apple", "banana", "cherry")
+print(myTuple)
+print(type(myTuple))
+
+#Set
+mySet = {"apple", "banana", "cherry"}
+print(mySet)
+print(type(mySet))
+
+
+
+
+
+
---
+++ PYTHON/01_Types and Operations/04_TuplesFilesandEverythingElse.py
@@ -0,0 +1,4 @@
+myTuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+# Access Tuple Items
+print(thistuple[1])
