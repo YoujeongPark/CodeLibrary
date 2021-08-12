e795f7029006d79774be21fe4fae2222accf1d54
youjeongPark <pyj0695@gmail.com>
2021-08-12 23:02:28 +0900

    sorting class

---
+++ JAVA/src/SortingClass.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class SortingClass {
+
+
+    public static void main(String[] args){
+
+        String[] alphabets = {"Kim","Park","Yi", "park"};
+
+        Arrays.sort(alphabets);
+        System.out.println(Arrays.toString(alphabets));
+
+
+    }
+
+}
