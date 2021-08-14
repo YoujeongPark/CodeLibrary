edd59170b99193505975f6a53c2b3e8e548b5d7e
youjeongPark <pyj0695@gmail.com>
2021-08-14 22:53:46 +0900

    comparable implement

--- JAVA/src/SortingClass.java
+++ JAVA/src/SortingClass.java
@@ -14,6 +14,39 @@ public class SortingClass {
         Arrays.sort(alphabets, String.CASE_INSENSITIVE_ORDER);
         System.out.println(Arrays.toString(alphabets));
 
+
+        Name[] name = new Name[]{
+            new Name("rosy", 1993),
+            new Name("lisa", 1998)
+        };
+
+        Arrays.sort(name);
+        for(Name n : name){
+            System.out.println(n);
+        }
+
+
+    }
+
+    static class Name implements Comparable<Object>{
+        String name;
+        int birthYear;
+
+        public Name(String name, int birthYear){
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public String toString(){
+            return name + " " + birthYear + "년생";
+        }
+
+        @Override
+        public int compareTo(Object object){
+            return name.compareTo(((Name) object).name);
+        }
+
+
     }
 
 }
